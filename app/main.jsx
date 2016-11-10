94080cd4108bfe9d8ec15940aa6bf5eb301a67ce
Jordan Lysenko <Jordan@Jordans-MacBook-Air.local>
2016-11-10 15:46:25 -0500

    base timer component

--- app/main.jsx
+++ app/main.jsx
@@ -4,16 +4,17 @@ import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
-import store from './store'
+import store from './store';
+import Timer from './components/Timer';
 
 
 
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+      <Route path="/" component={Timer}>
+        <IndexRedirect to="/timer" />
+        <Route path="/timer" component={Timer} />
       </Route>
     </Router>
   </Provider>,
--- public/bundle.js
+++ public/bundle.js
@@ -60,6 +60,10 @@
 	
 	var _store2 = _interopRequireDefault(_store);
 	
+	var _Timer = __webpack_require__(290);
+	
+	var _Timer2 = _interopRequireDefault(_Timer);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	(0, _reactDom.render)(_react2.default.createElement(
@@ -70,9 +74,9 @@
 	    { history: _reactRouter.browserHistory },
 	    _react2.default.createElement(
 	      _reactRouter.Route,
-	      { path: '/', component: ExampleApp },
-	      _react2.default.createElement(_reactRouter.IndexRedirect, { to: '/jokes' }),
-	      _react2.default.createElement(_reactRouter.Route, { path: '/jokes', component: Jokes })
+	      { path: '/', component: _Timer2.default },
+	      _react2.default.createElement(_reactRouter.IndexRedirect, { to: '/timer' }),
+	      _react2.default.createElement(_reactRouter.Route, { path: '/timer', component: _Timer2.default })
 	    )
 	  )
 	), document.getElementById('main'));
@@ -30469,6 +30473,90 @@
 	thunk.withExtraArgument = createThunkMiddleware;
 	
 	exports['default'] = thunk;
+
+/***/ },
+/* 290 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Timer = function (_Component) {
+	  _inherits(Timer, _Component);
+	
+	  function Timer() {
+	    _classCallCheck(this, Timer);
+	
+	    return _possibleConstructorReturn(this, (Timer.__proto__ || Object.getPrototypeOf(Timer)).apply(this, arguments));
+	  }
+	
+	  _createClass(Timer, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        { className: 'timer' },
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'timebox' },
+	          _react2.default.createElement(
+	            'div',
+	            null,
+	            'img goes here'
+	          )
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'timebox' },
+	          _react2.default.createElement(
+	            'div',
+	            null,
+	            'img goes here'
+	          )
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'timebox' },
+	          _react2.default.createElement(
+	            'div',
+	            null,
+	            'img goes here'
+	          )
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'timebox' },
+	          _react2.default.createElement(
+	            'div',
+	            null,
+	            'img goes here'
+	          )
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Timer;
+	}(_react.Component);
+	
+	exports.default = Timer;
 
 /***/ }
 /******/ ]);
